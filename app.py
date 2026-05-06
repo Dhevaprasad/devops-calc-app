7ad8dd2c2a8021d32eb823cdc64d195744ec2757
Dhevaprasad <79324862+Dhevaprasad@users.noreply.github.com>
2026-05-06 12:36:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import time
 
-def add(a,b):
-  return a+b
+def add(a, b):
+    return a + b
 
-def multiply(a,b):
-  return a*b
+def multiply(a, b):
+    return a * b
 
-print("Addition:", add(2,3))
-print("Multiply:", multiply(2,3))
+def main():
+    print("Addition:", add(2, 3))
+    print("Multiplication:", multiply(2, 3))
 
+    while True:
+        time.sleep(10)
 
+if __name__ == "__main__":
+    main()
