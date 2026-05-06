cc55c1fc19d09d5099ba9ff0dfd3e5efd8ca2821
Dhevaprasad <79324862+Dhevaprasad@users.noreply.github.com>
2026-05-06 11:49:27 +0530

    Add basic arithmetic functions and infinite loop

---
+++ app.py
@@ -0,0 +1,14 @@
+import time
+
+def add(a,b):
+  return a+b
+
+def multiply(a,b):
+  return a*b
+
+print("Addition:", add(2,3))
+print("Multiply:", multiply(2,3))
+
+while True:
+    time.sleep(10)
+
